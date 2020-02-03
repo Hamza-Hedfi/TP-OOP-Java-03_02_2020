0cad4bd77282b59fe91844628d9b1587499529de
Hamza Hedfi <hedfi.hamza@hotmail.fr>
2020-02-03 14:10:25 +0100

    Employee

---
+++ src/com/deuterium/exercice_3/MainEmployee.java
@@ -0,0 +1,9 @@
+package com.deuterium.exercice_3;
+
+public class MainEmployee {
+
+    public static void main(String[] args) {
+        Employee e = new Employee(1, "Hamza", "Hadfi", 9_999_999);
+        System.out.println(e);
+    }
+}
